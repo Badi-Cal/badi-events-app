38abf31db98fc565704290a685c2f4e2392eb03d
dmcblue <dmcblue@gmail.com>
2021-02-06 12:53:08 -0500

    Change deploy group name for better local management

--- config/make_hosts.py
+++ config/make_hosts.py
@@ -3,7 +3,7 @@ import os
 HOSTS = os.getenv('HOSTS')
 ips = HOSTS.split(" ")
 file = open("hosts", "w") 
-file.write("[badiCal]\n")
+file.write("[badiCalBadiEventsApp]\n")
 for index, ip in enumerate(ips):
 	file.write('inv%s ansible_ssh_host=%s\n'%(index, ip)) 
 file.close()
