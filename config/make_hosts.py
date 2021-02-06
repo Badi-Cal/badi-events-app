39d6599046691bf2c3c28e9f86b4cd16ad184dda
dmcblue <dmcblue@gmail.com>
2021-02-06 13:06:37 -0500

    Revert "Change deploy group name for better local management"
    This reverts commit 38abf31db98fc565704290a685c2f4e2392eb03d.

--- config/make_hosts.py
+++ config/make_hosts.py
@@ -3,7 +3,7 @@ import os
 HOSTS = os.getenv('HOSTS')
 ips = HOSTS.split(" ")
 file = open("hosts", "w") 
-file.write("[badiCalBadiEventsApp]\n")
+file.write("[badiCal]\n")
 for index, ip in enumerate(ips):
 	file.write('inv%s ansible_ssh_host=%s\n'%(index, ip)) 
 file.close()
