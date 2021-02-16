6f13ab4cd4850bf418733150f2808b9c77b4ec9c
Kenny Rogers <kennyr87@gmail.com>
2021-02-15 20:52:38 -0500

    refactor!: tests to remove full cal unit tests

--- test/node-loader.config.js
+++ test/node-loader.config.js
@@ -1,6 +1,5 @@
-import * as importMapLoader from "@node-loader/import-maps";
 import * as cssLoader from '../lib/css-loader.mjs';
 
 export default {
-  loaders: [cssLoader, importMapLoader]
+  loaders: [cssLoader]
 };
