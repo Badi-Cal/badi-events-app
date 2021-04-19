63389b77525ebacfeed567892709bc880acba82d
Kenny Rogers <kennyr87@gmail.com>
2021-04-19 15:43:30 -0400

    refactor!: remove full calendar modules, tests

--- test/tests/mocha.test.js
+++
@@ -1,9 +0,0 @@
-import 'chai/register-expect';
-import assert from 'assert';
-
-// Smoke test to ensure Mocha dependencies are properly in place
-describe('This Mocha test enviroment', function() {
-  it('should be able to execute a test', function(){
-    assert.ok(expect);
-  });
-});
