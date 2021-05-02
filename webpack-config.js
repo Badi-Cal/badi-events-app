e56baebbe549a0bc4d5f71d65cd6ddaff4ac377f
Kenny Rogers <kennyr87@gmail.com>
2021-05-01 21:13:22 -0400

    build: configure stylus loader; update boot alias

--- webpack-config.js
+++ webpack-config.js
@@ -3,12 +3,22 @@ const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = function (cfg) {
-  cfg.module.rules.push({
-    enforce: 'pre',
-    test: /\.(js|vue)$/,
-    loader: 'eslint-loader',
-    exclude: /(node_modules|quasar)/
-  })
+  cfg.module.rules.push(
+    {
+      enforce: 'pre',
+      test: /\.(js|vue)$/,
+      loader: 'eslint-loader',
+      exclude: /(node_modules|quasar)/
+    },
+    {
+      test: /\.styl$/,
+      loader: 'stylus-loader', // compiles Styl to CSS
+      include: [
+        path.resolve(__dirname, './src/css'),
+        path.resolve(__dirname, './component/calendar/styles-common')
+      ]
+    }
+  )
   cfg.resolve.alias = {
     ...cfg.resolve.alias,
     src: path.resolve(__dirname, './src'),
@@ -16,11 +26,12 @@ module.exports = function (cfg) {
     layouts: path.resolve(__dirname, './src/layouts'),
     pages: path.resolve(__dirname, './src/pages'),
     assets: path.resolve(__dirname, './src/assets'),
-    boot: path.resolve(__dirname, './src/boot')
+    boot: path.resolve(__dirname, './boot')
   }
   cfg.plugins.push(
     new CopyPlugin([
       { from: 'src/statics', to: 'statics' }
     ])
   )
+  return cfg
 }
