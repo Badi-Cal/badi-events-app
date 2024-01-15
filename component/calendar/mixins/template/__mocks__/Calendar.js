506ec2460dc63ff10c837961f1e24495acc59346
Kenny Rogers <kennyr87@gmail.com>
2024-01-14 19:22:26 -0500

    test: fix properties for CalendarTemplateMixin mock

--- component/calendar/mixins/template/__mocks__/Calendar.js
+++ component/calendar/mixins/template/__mocks__/Calendar.js
@@ -7,18 +7,10 @@ export const spyEventsHandling = jest.fn()
 export default {
   __esModule: true,
   default: {
-    computed: {
-      ...mixin.default.computed
-    },
+    ...mixin.default,
     methods: {
       ...mixin.default.methods,
       setupEventsHandling: spyEventsHandling
-    },
-    props: {
-      ...mixin.default.props
-    },
-    mounted () {
-      mixin.default.mounted.call(this)
     }
   }
 }
