bf78fb242fb506168db78453c3bb5185932aa53f
Kenny Rogers <kennyr87@gmail.com>
2024-01-17 15:01:47 -0500

    feat: add constructor for BadDateAPI class

--- utils/badidate.js
+++ utils/badidate.js
@@ -6,6 +6,12 @@
 import { BadiDate } from 'badidate'
 
 export default class BadiDateAPI extends BadiDate {
+  constructor (date) {
+    if (date === undefined) {
+      date = new Date()
+    }
+    super(date)
+  }
   plus (object) {
     let date = this.gregorianDate.plus(object)
     return new BadiDateAPI(date)
