d401bdf729baacbf83bdb2e70a9e46e370721641
Kenny Rogers <kennyr87@gmail.com>
2024-01-18 14:06:01 -0500

    test: refactor Calendar.spec.js tests

--- test/jest/__tests__/Calendar.spec.js
+++ test/jest/__tests__/Calendar.spec.js
@@ -1,9 +1,7 @@
-import { createWrapper, shallowMount, mount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import Vue from 'vue'
-import { GregorianCalendar } from '../../../component/quasar'
+import { Calendar } from '../../../component/quasar'
 import Quasar from '../utils'
-
-import { DateTime } from 'luxon'
 
 // default module is on named default property of export object
 import CalendarTemplateMixin, { spyEventsHandling } from 'mixins/template/Calendar'
@@ -18,7 +16,7 @@ describe('Calendar', () => {
     let wrapper
 
     beforeEach(() => {
-      wrapper = mount(GregorianCalendar, {
+      wrapper = mount(Calendar, {
         LocalVue,
         stubs: ['router-link'],
         mixins: [CalendarTemplateMixin.default]
@@ -32,7 +30,7 @@ describe('Calendar', () => {
     })
 
     it('should be a Vue instance', () => {
-      const instance = wrapper.findComponent(GregorianCalendar)
+      const instance = wrapper.findComponent(Calendar)
       expect(instance.exists()).toBe(true)
     })
 
@@ -40,65 +38,6 @@ describe('Calendar', () => {
       const vm = wrapper.vm
       expect(typeof vm.setupEventsHandling).toBe('function')
       expect(spyEventsHandling).toHaveBeenCalled()
-    })
-  })
-
-  describe('component data object', () => {
-    let wrapper
-    beforeEach(() => {
-      wrapper = shallowMount(GregorianCalendar, {
-        LocalVue,
-        propsData: {
-          startDate: new Date()
-        },
-        mixins: [CalendarTemplateMixin.default]
-      })
-    })
-
-    afterEach(() => {
-      wrapper.destroy()
-    })
-
-    it('should create correct working date', () => {
-      const vm = wrapper.vm
-      expect(vm.$data.workingDate instanceof Date).toBe(true)
-      expect(vm.workingDateGregorian instanceof DateTime).toBe(true)
-
-      const date = DateTime.fromJSDate(new Date())
-      expect(vm.workingDateGregorian.toLocaleString()).toBe(date.toLocaleString())
-    })
-  })
-
-  describe.skip('component methods', () => {
-    let wrapper
-    beforeEach(() => {
-      wrapper = shallowMount(GregorianCalendar, {
-        LocalVue,
-        propsData: {
-          startDate: new Date()
-        },
-        mixins: [CalendarTemplateMixin.default]
-      })
-    })
-
-    afterEach(() => {
-      wrapper.destroy()
-    })
-
-    // TODO: move test to CalenderHeader
-    it('moveTimePeriod - should set the workingDateGregorian computed property', () => {
-      const vm = wrapper.vm
-      const fixture = {
-        unitType: 'month',
-        amount: 1
-      }
-      let paramObj = {}
-      paramObj[fixture.unitType] = fixture.amount
-
-      const testDate = DateTime.fromJSDate(vm.$props.startDate).plus(paramObj)
-      vm.moveTimePeriod(fixture)
-
-      expect(vm.workingDateGregorian.toLocaleString()).toBe(testDate.toLocaleString())
     })
   })
 })
